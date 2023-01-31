75c2216dd6a0023f6e8d9f8f5a0cfd1369165e06
JavierReyes <javjreyes@gmail.com>
2023-01-31 14:25:29 -0600

    create ticTacToe module and impliment turn function to determine which player is active and insert coresponding symbol in grid cell

---
+++ ticTacToe.js
@@ -0,0 +1,23 @@
+const ticTacToe = (() =>{
+    let player1Score=0;
+    let player2Score=0;
+    let turnTracker=0;
+    let whosTurn="player1";
+
+    const turn = cellChoice => {
+        let choice;
+        if(whosTurn=="player1"){
+            choice="X";
+            whosTurn="player2";
+        }
+        else{
+            choice="O";
+            whosTurn="player1";
+        }
+        cellChoice.innerText=choice;
+
+    }
+    return{
+        turn
+    };
+})();
